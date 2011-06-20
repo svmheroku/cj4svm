1eb2bd113d55ec60e3031e80b2bb9c0a02e481c3
svm heroku <svmheroku@yahoo.com>
2011-06-20 07:02:18 +0100

    wgetit.bash

--- bin/wgetit.bash
+++ bin/wgetit.bash
@@ -9,15 +9,15 @@ cd /tmp/
 rm -f predictions fx us_stk fx_new fx_past us_stk_new us_stk_past
 rm -f predictions.? fx.? us_stk.? fx_new.? fx_past.? us_stk_new.? us_stk_past.?
 
-wget http://svm.heroku.com/predictions
+wget http://roboluck.com/predictions
 
-wget http://svm.heroku.com/predictions/fx
-wget http://svm.heroku.com/predictions/us_stk
+wget http://roboluck.com/predictions/fx
+wget http://roboluck.com/predictions/us_stk
 
-wget http://svm.heroku.com/fx_new
-wget http://svm.heroku.com/fx_past
+wget http://roboluck.com/fx_new
+wget http://roboluck.com/fx_past
 
-wget http://svm.heroku.com/us_stk_new
-wget http://svm.heroku.com/us_stk_past
+wget http://roboluck.com/us_stk_new
+wget http://roboluck.com/us_stk_past
 
 exit 0

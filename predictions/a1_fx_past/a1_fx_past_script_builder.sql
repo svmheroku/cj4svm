cd1428a50d6373c9304e5ce382c7e6d316acd980
svm heroku <svmheroku@yahoo.com>
2011-07-24 04:10:52 +0100

    a1_fx_past/index_spec.rb

--- predictions/a1_fx_past/a1_fx_past_script_builder.sql
+++ predictions/a1_fx_past/a1_fx_past_script_builder.sql
@@ -5,7 +5,7 @@
 ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD';
 
 SPOOL /tmp/a1_fx_past_weeks.txt
-SELECT '@/tmp/a1_fx_past_week.sql '||MIN(ydate) cmd
+SELECT '@a1_fx_past_week.sql '||MIN(ydate) cmd
 FROM fxpst12
 WHERE price_6hr > 0
 -- I only want recent data

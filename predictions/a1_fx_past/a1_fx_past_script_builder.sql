a35d16c483d5473dccfa12e4db5dd5d52151aa94
svm heroku <svmheroku@yahoo.com>
2011-07-24 07:03:33 +0100

    a1_fx_past_script_builder.sql

--- predictions/a1_fx_past/a1_fx_past_script_builder.sql
+++ predictions/a1_fx_past/a1_fx_past_script_builder.sql
@@ -9,7 +9,7 @@ SELECT '@a1_fx_past_week.sql '||MIN(ydate) cmd
 FROM fxpst12
 WHERE price_6hr > 0
 -- I only want recent data
-AND ydate=(SELECT MAX(ydate)FROM fxpst12 WHERE TO_CHAR(ydate,'Dy')='Sun')
+-- AND ydate=(SELECT MAX(ydate)FROM fxpst12 WHERE TO_CHAR(ydate,'Dy')='Sun')
 GROUP BY TO_CHAR(ydate,'WW')
 ORDER BY MIN(ydate)
 /

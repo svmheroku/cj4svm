d6aaa1780e218863b00394bee41986fa0b7a133a
svm heroku <svmheroku@yahoo.com>
2011-07-24 04:50:15 +0100

    index_spec.rb

--- predictions/a1_fx_past/a1_fx_past_week.sql
+++ predictions/a1_fx_past/a1_fx_past_week.sql
@@ -25,6 +25,25 @@ HAVING(STDDEV(g5n) > 0)
 ORDER BY pair
 /
 
+SELECT
+pair
+,ROUND(AVG(score_diff),2) avg_danbot_score
+,ROUND(AVG(g5n) / STDDEV(g5n),2) sharpe_ratio
+,ROUND(AVG(g5n),4)   avg_5hr_n_gain
+,COUNT(g5n)          prediction_count
+,ROUND(SUM(g5n),4)   sum_5hr_n_gain
+FROM fxpst12
+WHERE rnng_crr1 > 0.1
+AND score_diff > 0.55
+AND ydate > '&1'
+AND ydate - 7 < '&1'
+AND g1 < 0.0004
+AND price_6hr > 0
+GROUP BY pair
+HAVING(STDDEV(g5n) > 0)
+ORDER BY pair
+/
+
 
 SPOOL OFF
 SET MARKUP HTML OFF

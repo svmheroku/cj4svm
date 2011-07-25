578841de0d984d1d37ab39e7fd681ae03f9430eb
svm heroku <svmheroku@yahoo.com>
2011-07-25 05:15:12 +0100

    a1_us_stk_past/index_spec.rb

---
+++ predictions/a1_us_stk_past/a1_us_stk_past_script_builder.sql
@@ -0,0 +1,22 @@
+--
+-- a1_us_stk_past_script_builder.sql
+--
+
+-- This SELECT gives me syntax to run a series of SQL scripts.
+-- Each script will give me data for 1 week.
+
+ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD';
+
+SPOOL /tmp/a1_us_stk_past_weeks.txt
+
+SELECT
+'@a1_us_stk_past_week.sql '||MIN(ydate) cmd
+FROM us_stk_pst13
+WHERE price_24hr > 0
+GROUP BY TO_CHAR(ydate,'WW')
+ORDER BY MIN(ydate)
+/
+
+SPOOL OFF
+
+exit

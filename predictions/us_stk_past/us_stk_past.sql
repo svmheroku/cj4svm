164236bd6e4bbcb1e8be1a7323a03432523155ef
svm heroku <svmheroku@yahoo.com>
2011-10-08 23:05:43 +0100

    us_stk_past

--- predictions/us_stk_past/us_stk_past.sql
+++ predictions/us_stk_past/us_stk_past.sql
@@ -2,165 +2,210 @@
 -- us_stk_past.sql
 --
 
--- This script creates tables used by:
--- us_stk_past_week.sql
--- which is called by:
--- /tmp/run_us_stk_past_week.sql
--- which is both created and called by us_stk_past/index_spec.rb
+-- I use this script to join gains with scores.
 
--- Assume that stkscores21 table exists due to an earlier import of data from other DBs:
-INSERT INTO stkscores21
-(
-tkr
-,ydate
-,tkrdate
-,targ
-,score
-)
-SELECT
-tkr
-,ydate
-,tkrdate
-,targ
-,score
-FROM stkscores
-ORDER BY targ,tkrdate
-/
+-- I need dates to match-up to the second:
+ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD hh24:mi:ss';
 
--- Now merge scores into stkscores23:
-DROP TABLE stkscores23;
-CREATE TABLE stkscores23 COMPRESS AS
-SELECT
-tkrdate
-,targ
-,MAX(tkr)tkr
-,MAX(ydate)ydate
-,AVG(score)score
-FROM stkscores21
-GROUP BY targ,tkrdate
-ORDER BY targ,tkrdate
-/
+-- Start by getting a copy of stkscores:
 
--- Assume that us_stk_pst21 table exists due to an earlier import of data from other DBs:
-INSERT INTO us_stk_pst21
-(
-tkr
-,ydate
-,tkrdate
-,price_0hr
-,price_24hr
-,g24hr
-,selldate
-,price_1hr
-,g1hr
-)
+PURGE RECYCLEBIN;
+DROP TABLE ibs5min_sun1;
+
+DROP TABLE ibs5min_sun;
+
+DROP TABLE stkscores_sunl;
+
+DROP TABLE stkscores_suns;
+
+DROP TABLE stkscores_sunlj;
+
+DROP TABLE stkscores_sunsj;
+
+DROP TABLE us_stk_sunday_l;
+
+DROP TABLE us_stk_sunday_s;
+
+CREATE TABLE ibs5min_sun1 COMPRESS AS
 SELECT
-tkr
-,ydate
-,tkrdate
-,clse  price_0hr
-,clse2 price_24hr
-,gain1day g24hr
-,selldate
-,LEAD(clse,12*1,NULL)OVER(PARTITION BY tkr ORDER BY ydate) price_1hr
-,(LEAD(clse,12*1,NULL)OVER(PARTITION BY tkr ORDER BY ydate)-clse) g1hr
-FROM di5min_stk_c2
-WHERE clse > 0
+tkr,ydate
+,AVG(clse)clse
+FROM ibs5min
+GROUP BY tkr,ydate
 ORDER BY tkr,ydate
 /
 
--- Now filter duplicates out of us_stk_pst21 into us_stk_pst11
-
-DROP TABLE us_stk_pst11;
-CREATE TABLE us_stk_pst11 COMPRESS AS
+CREATE TABLE ibs5min_sun COMPRESS AS
 SELECT
-tkrdate
-,MAX(tkr)tkr
-,MAX(ydate)ydate
-,MAX(selldate)selldate
-,AVG(price_0hr)price_0hr
-,AVG(price_24hr)price_24hr
-,AVG(g24hr)g24hr
-,AVG(price_1hr)price_1hr
-,AVG(g1hr)g1hr
-FROM us_stk_pst21
-GROUP BY tkrdate
-ORDER BY tkrdate
+tkr
+,ydate
+,clse                                                           price_0hr
+, LEAD(clse,12*1,NULL)OVER(PARTITION BY tkr ORDER BY ydate)     price_1hr
+,(LEAD(clse,12*1,NULL)OVER(PARTITION BY tkr ORDER BY ydate)-clse) g1hr
+,LEAD(clse, 6.5*12*1,NULL)OVER(PARTITION BY tkr ORDER BY ydate) price_24hr
+,LEAD(ydate,6.5*12*1,NULL)OVER(PARTITION BY tkr ORDER BY ydate) selldate
+,(LEAD(clse,6.5*12*1,NULL)OVER(PARTITION BY tkr ORDER BY ydate)-clse) g24hr
+FROM ibs5min_sun1
+ORDER BY tkr,ydate
 /
 
-ANALYZE TABLE us_stk_pst11 ESTIMATE STATISTICS SAMPLE 9 PERCENT;
-
--- Now join us_stk_pst11 with stkscores23
-
-DROP TABLE us_stk_pst13;
-PURGE RECYCLEBIN;
-CREATE TABLE us_stk_pst13 COMPRESS AS
+CREATE TABLE stkscores_sunl COMPRESS AS
 SELECT
-m.tkr
-,m.ydate
-,(l.score-s.score)        score_diff
-,ROUND(l.score-s.score,1) rscore_diff1
-,ROUND(l.score-s.score,2) rscore_diff2
-,m.g1hr
-,m.g24hr
-,m.selldate
-,m.price_0hr
-,m.price_1hr
-,m.price_24hr
-,COVAR_POP(l.score-s.score,m.g24hr)OVER(PARTITION BY l.tkr ORDER BY l.ydate ROWS BETWEEN 12*24*5 PRECEDING AND CURRENT ROW)rnng_crr1
-FROM stkscores23 l,stkscores23 s,us_stk_pst11 m
-WHERE l.targ='gatt'
-AND   s.targ='gattn'
-AND l.tkrdate = s.tkrdate
-AND l.tkrdate = m.tkrdate
+tkr
+,ydate
+,AVG(score)score
+FROM stkscores
+WHERE targ = 'gatt'
+GROUP BY tkr,ydate
+ORDER BY tkr,ydate
 /
 
-ANALYZE TABLE us_stk_pst13 ESTIMATE STATISTICS SAMPLE 9 PERCENT;
+CREATE TABLE stkscores_suns COMPRESS AS
+SELECT
+tkr
+,ydate
+,AVG(score)score
+FROM stkscores
+WHERE targ = 'gattn'
+GROUP BY tkr,ydate
+ORDER BY tkr,ydate
+/
+
+-- Now join em to get bullish scores:
+
+CREATE TABLE stkscores_sunlj COMPRESS AS
+SELECT
+l.tkr
+,l.ydate
+,l.score - s.score score
+FROM stkscores_sunl l, stkscores_suns s
+WHERE l.tkr||l.ydate = s.tkr||s.ydate
+AND l.score - s.score > 0.55
+AND l.score > 0.55
+AND s.score < 0.45
+ORDER BY s.tkr,s.ydate
+/
+
+-- Now join em to get bearish scores:
+
+CREATE TABLE stkscores_sunsj COMPRESS AS
+SELECT
+l.tkr
+,l.ydate
+,l.score - s.score score
+FROM stkscores_sunl l, stkscores_suns s
+WHERE l.tkr||l.ydate = s.tkr||s.ydate
+AND l.score - s.score < -0.55
+AND s.score > 0.55
+AND l.score < 0.45
+ORDER BY s.tkr,s.ydate
+/
+
+-- Indexes help speed up future joins:
+CREATE INDEX ibs5min_suni1 ON ibs5min_sun(tkr,ydate);
+CREATE INDEX stkscores_sunsji1 ON stkscores_sunsj(tkr,ydate);
+CREATE INDEX stkscores_sunlji1 ON stkscores_sunlj(tkr,ydate);
+
+-- Statistics help speed up future joins:
+ANALYZE TABLE ibs5min_sun     ESTIMATE STATISTICS SAMPLE 11 PERCENT;
+ANALYZE TABLE stkscores_sunsj ESTIMATE STATISTICS SAMPLE 11 PERCENT;
+ANALYZE TABLE stkscores_sunlj ESTIMATE STATISTICS SAMPLE 11 PERCENT;
 
 -- rpt
--- This SELECT gives me a list of recent week-names.
--- I use minday, maxday to help me understand the contents of each week.
+SELECT MIN(ydate),                      COUNT(ydate),MAX(ydate)FROM ibs5min_sun;
+SELECT MIN(ydate),MIN(score),MAX(score),COUNT(ydate),MAX(ydate)FROM stkscores_sunlj;
+SELECT MIN(ydate),MIN(score),MAX(score),COUNT(ydate),MAX(ydate)FROM stkscores_sunsj;
+
+-- Now join bullish scores with gains:
+
+CREATE TABLE us_stk_sunday_l COMPRESS AS
 SELECT
-TO_CHAR(ydate,'WW')
-,MIN(ydate)
-,TO_CHAR(MIN(ydate),'Dy')minday
-,COUNT(ydate)
-,MAX(ydate)
-,TO_CHAR(MAX(ydate),'Dy')maxday
-FROM us_stk_pst13
-WHERE price_24hr > 0
-GROUP BY TO_CHAR(ydate,'WW')
-ORDER BY MIN(ydate)
+i.tkr
+,i.ydate
+,price_0hr
+,price_1hr
+,g1hr
+,price_24hr
+,selldate
+,g24hr
+,score
+FROM ibs5min_sun i, stkscores_sunlj s
+WHERE i.tkr = s.tkr AND i.ydate = s.ydate
+ORDER BY i.tkr,i.ydate
 /
 
--- This SELECT gives me text for a-tags
-ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD';
-SET TIME off TIMING off ECHO off HEADING off
-SET MARKUP HTML ON TABLE "id='table_us_stk_past'" ENTMAP ON
-SPOOL /tmp/_us_stk_past_spool.html.erb
+-- Now join bearish scores with gains:
+
+CREATE TABLE us_stk_sunday_s COMPRESS AS
+SELECT
+i.tkr
+,i.ydate
+,price_0hr
+,price_1hr
+,g1hr
+,price_24hr
+,selldate
+,g24hr
+,score
+FROM ibs5min_sun i, stkscores_sunsj s
+WHERE i.tkr = s.tkr AND i.ydate = s.ydate
+ORDER BY i.tkr,i.ydate
+/
+
+-- Get bullish CSV data:
+SPOOL /tmp/us_stk_sunday_l.txt
+
+COLUMN sum_g24hr FORMAT 9999999.99
+COLUMN cum_sum   FORMAT 9999999.99
+SET COLSEP ","
 
 SELECT
-'Week: '||MIN(ydate)||' Through '||MAX(ydate) wweek
-FROM us_stk_pst13
-WHERE price_24hr > 0
-GROUP BY TO_CHAR(ydate,'WW')
-ORDER BY MIN(ydate)
+wk
+,TO_CHAR(week_of,'YYYY-MM-DD')  week_of
+,rownum                         rrownum
+,prediction_count
+,sum_g24hr
+,SUM(sum_g24hr)OVER(ORDER BY wk)cum_sum
+FROM
+(
+  SELECT
+  TO_CHAR(ydate,'YYYY-WW') wk
+  ,MIN(ydate)              week_of
+  ,COUNT(g24hr)            prediction_count
+  ,SUM(g24hr)              sum_g24hr
+  FROM us_stk_sunday_l
+  WHERE ydate > '2011-01-01'
+  GROUP BY TO_CHAR(ydate,'YYYY-WW')
+  ORDER BY TO_CHAR(ydate,'YYYY-WW')
+)
 /
 
 SPOOL OFF
-SET MARKUP HTML OFF
 
--- This SELECT gives me syntax to run a series of SQL scripts.
--- Each script will give me data for 1 week.
+-- Get bearish CSV data:
+SPOOL /tmp/us_stk_sunday_s.txt
 
-SPOOL /tmp/us_stk_past_week.txt
 SELECT
-'@us_stk_past_week.sql '||MIN(ydate) cmd
-FROM us_stk_pst13
-WHERE price_24hr > 0
-GROUP BY TO_CHAR(ydate,'WW')
-ORDER BY MIN(ydate)
+wk
+,TO_CHAR(week_of,'YYYY-MM-DD')  week_of
+,rownum                         rrownum
+,prediction_count
+,sum_g24hr
+,SUM(sum_g24hr)OVER(ORDER BY wk)cum_sum
+FROM
+(
+  SELECT
+  TO_CHAR(ydate,'YYYY-WW') wk
+  ,MIN(ydate)              week_of
+  ,COUNT(g24hr)            prediction_count
+  ,SUM(g24hr)              sum_g24hr
+  FROM us_stk_sunday_s
+  WHERE ydate > '2011-01-01'
+  GROUP BY TO_CHAR(ydate,'YYYY-WW')
+  ORDER BY TO_CHAR(ydate,'YYYY-WW')
+)
 /
+
 SPOOL OFF
 
 exit
---
+++ predictions/us_stk_past/us_stk_past_old.sql
@@ -0,0 +1,166 @@
+--
+-- us_stk_past.sql
+--
+
+-- This script creates tables used by:
+-- us_stk_past_week.sql
+-- which is called by:
+-- /tmp/run_us_stk_past_week.sql
+-- which is both created and called by us_stk_past/index_spec.rb
+
+-- Assume that stkscores21 table exists due to an earlier import of data from other DBs:
+INSERT INTO stkscores21
+(
+tkr
+,ydate
+,tkrdate
+,targ
+,score
+)
+SELECT
+tkr
+,ydate
+,tkrdate
+,targ
+,score
+FROM stkscores
+ORDER BY targ,tkrdate
+/
+
+-- Now merge scores into stkscores23:
+DROP TABLE stkscores23;
+CREATE TABLE stkscores23 COMPRESS AS
+SELECT
+tkrdate
+,targ
+,MAX(tkr)tkr
+,MAX(ydate)ydate
+,AVG(score)score
+FROM stkscores21
+GROUP BY targ,tkrdate
+ORDER BY targ,tkrdate
+/
+
+-- Assume that us_stk_pst21 table exists due to an earlier import of data from other DBs:
+INSERT INTO us_stk_pst21
+(
+tkr
+,ydate
+,tkrdate
+,price_0hr
+,price_24hr
+,g24hr
+,selldate
+,price_1hr
+,g1hr
+)
+SELECT
+tkr
+,ydate
+,tkrdate
+,clse  price_0hr
+,clse2 price_24hr
+,gain1day g24hr
+,selldate
+,LEAD(clse,12*1,NULL)OVER(PARTITION BY tkr ORDER BY ydate) price_1hr
+,(LEAD(clse,12*1,NULL)OVER(PARTITION BY tkr ORDER BY ydate)-clse) g1hr
+FROM di5min_stk_c2
+WHERE clse > 0
+ORDER BY tkr,ydate
+/
+
+-- Now filter duplicates out of us_stk_pst21 into us_stk_pst11
+
+DROP TABLE us_stk_pst11;
+CREATE TABLE us_stk_pst11 COMPRESS AS
+SELECT
+tkrdate
+,MAX(tkr)tkr
+,MAX(ydate)ydate
+,MAX(selldate)selldate
+,AVG(price_0hr)price_0hr
+,AVG(price_24hr)price_24hr
+,AVG(g24hr)g24hr
+,AVG(price_1hr)price_1hr
+,AVG(g1hr)g1hr
+FROM us_stk_pst21
+GROUP BY tkrdate
+ORDER BY tkrdate
+/
+
+ANALYZE TABLE us_stk_pst11 ESTIMATE STATISTICS SAMPLE 9 PERCENT;
+
+-- Now join us_stk_pst11 with stkscores23
+
+DROP TABLE us_stk_pst13;
+PURGE RECYCLEBIN;
+CREATE TABLE us_stk_pst13 COMPRESS AS
+SELECT
+m.tkr
+,m.ydate
+,(l.score-s.score)        score_diff
+,ROUND(l.score-s.score,1) rscore_diff1
+,ROUND(l.score-s.score,2) rscore_diff2
+,m.g1hr
+,m.g24hr
+,m.selldate
+,m.price_0hr
+,m.price_1hr
+,m.price_24hr
+,COVAR_POP(l.score-s.score,m.g24hr)OVER(PARTITION BY l.tkr ORDER BY l.ydate ROWS BETWEEN 12*24*5 PRECEDING AND CURRENT ROW)rnng_crr1
+FROM stkscores23 l,stkscores23 s,us_stk_pst11 m
+WHERE l.targ='gatt'
+AND   s.targ='gattn'
+AND l.tkrdate = s.tkrdate
+AND l.tkrdate = m.tkrdate
+/
+
+ANALYZE TABLE us_stk_pst13 ESTIMATE STATISTICS SAMPLE 9 PERCENT;
+
+-- rpt
+-- This SELECT gives me a list of recent week-names.
+-- I use minday, maxday to help me understand the contents of each week.
+SELECT
+TO_CHAR(ydate,'WW')
+,MIN(ydate)
+,TO_CHAR(MIN(ydate),'Dy')minday
+,COUNT(ydate)
+,MAX(ydate)
+,TO_CHAR(MAX(ydate),'Dy')maxday
+FROM us_stk_pst13
+WHERE price_24hr > 0
+GROUP BY TO_CHAR(ydate,'WW')
+ORDER BY MIN(ydate)
+/
+
+-- This SELECT gives me text for a-tags
+ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD';
+SET TIME off TIMING off ECHO off HEADING off
+SET MARKUP HTML ON TABLE "id='table_us_stk_past'" ENTMAP ON
+SPOOL /tmp/_us_stk_past_spool.html.erb
+
+SELECT
+'Week: '||MIN(ydate)||' Through '||MAX(ydate) wweek
+FROM us_stk_pst13
+WHERE price_24hr > 0
+GROUP BY TO_CHAR(ydate,'WW')
+ORDER BY MIN(ydate)
+/
+
+SPOOL OFF
+SET MARKUP HTML OFF
+
+-- This SELECT gives me syntax to run a series of SQL scripts.
+-- Each script will give me data for 1 week.
+
+SPOOL /tmp/us_stk_past_week.txt
+SELECT
+'@us_stk_past_week.sql '||MIN(ydate) cmd
+FROM us_stk_pst13
+WHERE price_24hr > 0
+GROUP BY TO_CHAR(ydate,'WW')
+ORDER BY MIN(ydate)
+/
+SPOOL OFF
+
+exit
